076df174169d0926d1a44ebbc29028804bd24728
OstafiichukO <85893003+OstafiichukO@users.noreply.github.com>
2022-06-20 09:17:51 +0300

    [FIX]: function's naming fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,19 +4,19 @@ const router = express.Router();
 const { validateRequest } = require("../../middlewares/validateRequest");
 const { schemaSignup, schemaLogin } = require("../../models/user");
 const {
-  signup,
-  login,
-  logout,
-  current,
-  avatars,
+  signupUser,
+  loginUser,
+  logoutUser,
+  currentUser,
+  avatarsUser,
 } = require("../../controllers/auth");
 const { auth } = require("../../middlewares/auth");
 const upload = require("../../middlewares/upload");
 
-router.post("/signup", validateRequest(schemaSignup), signup);
-router.post("/login", validateRequest(schemaLogin), login);
-router.post("/logout", auth, logout);
-router.post("/current", auth, current);
-router.patch("/avatars", auth, upload.single("avatar"), avatars);
+router.post("/signup", validateRequest(schemaSignup), signupUser);
+router.post("/login", validateRequest(schemaLogin), loginUser);
+router.post("/logout", auth, logoutUser);
+router.post("/current", auth, currentUser);
+router.patch("/avatars", auth, upload.single("avatar"), avatarsUser);
 
 module.exports = router;
